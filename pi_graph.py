62f77b35ee6cbcdf4be7ef83724ff3ccc6cf0d0b
Haydon Murdoch <root0192@gmail.com>
2015-03-14 22:55:35 +0000

    Initial working commit

---
+++ pi_graph.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
+passes = 9
+num = 0
+char = ":"
+
+print(pi)
+
+for i in range(0, passes): # parse pi 10 times (0 to 9)
+    for j in range(0, len(pi)): # each parse, check each character individually
+        if pi[j] == ".": # if the checked char is the decimal point
+            print(" ", end="") # print a space
+        elif int(pi[j]) <= num: # if the checked char <= the number to check
+            print(" ", end="") # print a space
+        else: # ...
+            print(char, end="") # print a specific character
+
+    num += 1 # increment the number to check
+    print() # newline
+
+print() # newline
